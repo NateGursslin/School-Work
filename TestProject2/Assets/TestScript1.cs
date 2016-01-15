1016a354b5b55e48bf53492cb7e141e550dc5637
Nathan Gursslin <nathangursslin@gmail.com>
2016-01-14 18:01:46 -0700

    TestProject2, Variables and Functions

---
+++ TestProject2/Assets/TestScript1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript1 : MonoBehaviour {
+
+    int myInt = 13;
+
+    void Start() {
+
+        myInt = multiplyByItself(myInt);
+        Debug.Log(myInt);
+    }
+
+    int multiplyByItself(int number)
+
+   {
+    int ret;
+    ret = number * number;
+    return ret;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
