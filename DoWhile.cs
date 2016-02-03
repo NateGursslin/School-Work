e8fdb01352891a87af6fd494b45336db6feeceb0
NateGursslin <nathangursslin@gmail.com>
2016-02-02 19:01:33 -0700

    If Statements and Loops

---
+++ DoWhile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoWhile : MonoBehaviour {
+
+	
+	void Start ()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Continuing!");
+        }
+        while (shouldContinue == true);
+	
+	}
+	
+	
+	void Update () {
+	
+	}
+}
