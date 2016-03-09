3bab0e556ff04fb2ad16c6e613230168984600e1
NateGursslin <nathangursslin@gmail.com>
2016-03-08 18:51:43 -0700

    Midterm Projct

---
+++ Midterm Project/Assets/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float rotationSpeed = 3;
+    public float movementSpeed = 50;
+    public Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -rotationSpeed, 0));
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, rotationSpeed, 0));
+        }
+
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddRelativeForce(0, 0, movementSpeed);
+        }
+
+    }
+}
---
+++ Midterm Project/Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public float time = 25;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckTimer();
+	}
+
+    void CheckTimer ()
+    {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
